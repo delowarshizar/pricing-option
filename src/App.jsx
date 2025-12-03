907fdce20c28b335366cbc6e1fdfae0d9fd70ef5
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-04 00:31:48 +0600

    all extention initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import DaisyNav from "./components/Daisy/DaisyNav";
 
 function App() {
- 
-
   return (
     <>
-  
-      <h1>Vite + React</h1>
+    <DaisyNav></DaisyNav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
