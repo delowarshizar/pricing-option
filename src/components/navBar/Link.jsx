34df22374498a93f5d8c67307edee5d6062c76a2
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-05 22:08:39 +0600

    all fileinitialized

--- src/components/navBar/Link.jsx
+++ src/components/navBar/Link.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { TableOfContents } from "lucide-react";
-const Link = () => {
+const Link = ({route}) => {
   return (
     <div>
-
+        <li><a href={route.path}>{route.name}</a></li>
     </div>
   );
 };
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Link from './Link';
+
 
 const NavBar = () => {
+    const urls = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    name: "About Us",
+    path: "/about"
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services"
+  },
+  {
+    id: 4,
+    name: "Contact",
+    path: "/contact"
+  },
+  {
+    id: 5,
+    name: "Blog",
+    path: "/blog"
+  }
+];
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className='flex gap-3.5'>
+                {
+                    urls.map(route =><Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
     );
 };
 
